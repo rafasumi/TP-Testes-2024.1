541bcfd5740195a2c68742ad369bd6ab7c423977
Anne <anneisabelle.rc@gmail.com>
2024-04-30 23:07:50 -0300

    parte 8: autenticaçãoe  permissao - incompleto

--- catalog/models.py
+++ catalog/models.py
@@ -4,6 +4,8 @@ from django.db.models.functions import Lower
 from django.urls import reverse
 
 import uuid
+from django.conf import settings
+from datetime import date
 
 
 class Genre(models.Model):
@@ -58,6 +60,8 @@ class BookInstance(models.Model):
     due_back = models.DateField(null=True, blank=True)
     language = models.ForeignKey(
         'Language', on_delete=models.RESTRICT, null=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     LOAN_STATUS = (
         ('m', 'Em manutenção'),
@@ -76,9 +80,16 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
 
 
 class Author(models.Model):
