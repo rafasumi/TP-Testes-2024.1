9242a934d321b0ef83e2066f786ff260a407f02b
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2024-05-03 19:36:02 -0300

    Fixing problem with authentication templates

--- bib/urls.py
+++ bib/urls.py
@@ -23,11 +23,9 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True))
-]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL,
+                       document_root=settings.STATIC_ROOT)
