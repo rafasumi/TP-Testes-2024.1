1f7e2ec6f6ece7d9cb9521cf56e5eef7bf6e539f
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2024-05-06 17:58:41 -0300

    Translating texts

--- catalog/forms.py
+++ catalog/forms.py
@@ -13,12 +13,12 @@ class RenewBookModelForm(ModelForm):
 
         # Check if a date is not in the past.
         if data < datetime.date.today():
-            raise ValidationError(_('Invalid date - renewal in past'))
+            raise ValidationError(_('Data inválida - nova data já passou'))
 
         # Check if a date is in the allowed range (+4 weeks from today).
         if data > datetime.date.today() + datetime.timedelta(weeks=4):
             raise ValidationError(
-                _('Invalid date - renewal more than 4 weeks ahead'))
+                _('Data inválida - nova data é daqui a mais de 4 semanas'))
 
         # Remember to always return the cleaned data.
         return data
@@ -26,6 +26,6 @@ class RenewBookModelForm(ModelForm):
     class Meta:
         model = BookInstance
         fields = ['due_back']
-        labels = {'due_back': _('Renewal date')}
+        labels = {'due_back': _('Novo prazo')}
         help_texts = {'due_back': _(
-            'Enter a date between now and 4 weeks (default 3).')}
+            'Insira uma data entre hoje e daqui 4 semanas.')}
