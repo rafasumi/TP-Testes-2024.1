0e0f4eb4610ad6c324837da082919c15e1ab22aa
Anne <anneisabelle.rc@gmail.com>
2024-05-06 20:55:41 -0300

    adicionando mais alguns testes de modelo

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 
-from catalog.models import Author
+from catalog.models import Author, Genre, Book, BookInstance
+
+import datetime
+
+from django.utils import timezone
 
 class AuthorModelTest(TestCase):
     @classmethod
@@ -42,5 +46,94 @@ class AuthorModelTest(TestCase):
         author = Author.objects.get(id=1)
         # This will also fail if the urlconf is not defined.
         self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
+
+class GenreModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Genre.objects.create(name='Fantasia')
+
+    def test_genre_max_length(self):
+        genre = Genre.objects.get(id=1)
+        max_length = genre._meta.get_field('name').max_length
+        self.assertEqual(max_length, 200)
+
+    def test_genre_help_text(self):
+        genre = Genre.objects.get(id=1)
+        help_text = genre._meta.get_field('name').help_text
+        self.assertEqual(help_text, 'Insira um gênero literário')
+
+    #  testar o UniqueConstraint?
+        
+class BookModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Author.objects.create(first_name='Machado', last_name='de Assis')
+        author = Author.objects.get(id=1)
+
+        Genre.objects.create(name='Fantasia')
+        Genre.objects.create(name='Ficção Científica')
+        genre_objects_for_book = Genre.objects.all()
+        
+        test_book  = Book.objects.create(title='Dom Casmurro', author=author, summary='Summary Test', isbn='0000000')
+        test_book.genre.set(genre_objects_for_book)
+
+    def test_title_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'Título')
+
+    def test_author_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('author').verbose_name
+        self.assertEqual(field_label, 'Autor')
+
+    def test_summary_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('summary').verbose_name
+        self.assertEqual(field_label, 'Sumário')
+
+    def test_isbn_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('isbn').verbose_name
+        self.assertEqual(field_label,'ISBN')
+
+    def test_genre_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('genre').verbose_name
+        self.assertEqual(field_label,'Gênero')
+
+    def test_genre_display(self):
+        book = Book.objects.get(id=1)
+        self.assertEqual(str(book.display_genre()),'Fantasia, Ficção Científica')
+
+    def test_title_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field('title').max_length
+        self.assertEqual(max_length, 200)
+
+    def test_get_absolute_url(self):
+        book = Book.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        self.assertEqual(book.get_absolute_url(), '/catalog/book/1')
+
+class BookInstanceModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        
+        Author.objects.create(first_name='Machado', last_name='de Assis')
+        author = Author.objects.get(id=1)
+
+        Genre.objects.create(name='Fantasia')
+        Genre.objects.create(name='Ficção Científica')
+        genre_objects_for_book = Genre.objects.all()
+        
+        test_book  = Book.objects.create(title='Dom Casmurro', author=author, summary='Summary Test', isbn='0000000')
+        test_book.genre.set(genre_objects_for_book)
+
+        return_date = timezone.localtime() + datetime.timedelta(days=4)
+
+        BookInstance.objects.create(book=test_book, imprint='Unlikely Imprint, 2016', due_back=return_date, borrower='testuser1', status='r')
+
+
 
 
